3d8c633c546699bb4094abb7d62332fa9b15f191
Alexander Martinez <alexmtnezf@gmail.com>
2018-06-19 02:43:21 -0500

    Configuring gunicorn

--- run.py
+++ run.py
@@ -3,8 +3,16 @@ import multiprocessing
 from app import flaskApp as application
 from db import db
 
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get("PORT", 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
+
 # Gunicorn configs
-bind = "0.0.0.0:$(PORT)"
+bind = "0.0.0.0:{}".format(port)
 workers = multiprocessing.cpu_count() * 2 + 1
 
 # End
